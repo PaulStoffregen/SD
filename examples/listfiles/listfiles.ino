217792e6864ab087c4c2f50c21245e5a42cbf2cd
Frank <FrankBoesing@users.noreply.github.com>
2020-02-22 11:30:51 +0100

    Update listfiles.ino

--- examples/listfiles/listfiles.ino
+++ examples/listfiles/listfiles.ino
@@ -42,7 +42,7 @@ void setup()
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ; // wait for serial port to connect.
   }
 
 
