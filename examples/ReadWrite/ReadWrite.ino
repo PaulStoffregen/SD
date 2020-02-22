56fd2276cb4c8ae22c0135ed66c27f02ed353ccb
Frank <FrankBoesing@users.noreply.github.com>
2020-02-22 11:30:24 +0100

    Update ReadWrite.ino

--- examples/ReadWrite/ReadWrite.ino
+++ examples/ReadWrite/ReadWrite.ino
@@ -43,7 +43,7 @@ void setup()
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ; // wait for serial port to connect.
   }
 
 
@@ -55,8 +55,7 @@ void setup()
   }
   Serial.println("initialization done.");
   
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
+  // open the file.
   myFile = SD.open("test.txt", FILE_WRITE);
   
   // if the file opened okay, write to it:
