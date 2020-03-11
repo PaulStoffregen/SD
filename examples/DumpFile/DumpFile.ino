731309bc0761e48bd949d6e5bad3a588645f92f8
PaulStoffregen <paul@pjrc.com>
2020-03-10 20:24:42 -0700

    Update comments, fixes #21

--- examples/DumpFile/DumpFile.ino
+++ examples/DumpFile/DumpFile.ino
@@ -48,7 +48,7 @@ void setup()
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ; // wait for serial port to connect.
   }
 
 
@@ -62,8 +62,7 @@ void setup()
   }
   Serial.println("card initialized.");
   
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
+  // open the file.
   File dataFile = SD.open("datalog.txt");
 
   // if the file is available, write to it:
