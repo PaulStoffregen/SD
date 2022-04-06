468ba3c5bf283cd40a68acdfb25d3146bffcb79a
PaulStoffregen <paul@pjrc.com>
2022-04-06 07:03:16 -0700

    Add link to more wiring photos in SdFat_Usage example comments

--- examples/SdFat_Usage/SdFat_Usage.ino
+++ examples/SdFat_Usage/SdFat_Usage.ino
@@ -52,7 +52,8 @@ void setup()
   //ok = SD.sdfs.begin(SdSpiConfig(chipSelect, DEDICATED_SPI, SD_SCK_MHZ(16)));
 
   // Different SPI port (Teensy 4.1 SPI1 is MOSI1:pin26, MISO1:pin1, SCK1:pin27)
-  // photo: https://forum.pjrc.com/threads/69254?p=297875&viewfull=1#post297875
+  // SPI1 photo: https://forum.pjrc.com/threads/69254?p=297875&viewfull=1#post297875
+  // SPI2 photo: https://forum.pjrc.com/threads/60954?p=303782&viewfull=1#post303782
   //ok = SD.sdfs.begin(SdSpiConfig(chipSelect, SHARED_SPI, SD_SCK_MHZ(16), &SPI1));
 
   // Access the built in SD card on Teensy 3.5, 3.6, 4.1 using FIFO
